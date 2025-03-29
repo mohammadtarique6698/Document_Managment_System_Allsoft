7d3987a2923a7eca342b2322d9e97a44623a95bd
mohammadtarique6698 <mohammadtarique661998@gmail.com>
2025-03-29 21:42:46 +0530

    Download Functionality added.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
@@ -10,23 +8,21 @@ import Loader from "./components/Loader";
 import { SnackbarProvider } from "notistack";
 import "./index.css";
 import Navbar from "./components/Navbar";
-import FileUpload from "./components/FileUpload";
+// import FileUpload from "./components/FileUpload";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <SnackbarProvider maxSnack={3}>
-      <Provider store={store}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Loader />} />
-            <Route path="/generateOTP" element={<App />} />
-            <Route path="/validateOTP" element={<App />} />
-            <Route path="/home" element={<FileUpload />} />
-            <Route path="/admin" element={<Admin />} />
-          </Routes>
-        </Router>
-      </Provider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Loader />} />
+          <Route path="/generateOTP" element={<App />} />
+          <Route path="/validateOTP" element={<App />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
     </SnackbarProvider>
   </React.StrictMode>
 );
